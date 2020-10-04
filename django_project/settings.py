5455aabff9d2712a1cabfb2b6d5c97af5f7064f9
runnily <32201807+runnily@users.noreply.github.com>
2020-10-05 00:33:58 +0100

    added aws s3 bucket for file uploads

--- django_project/settings.py
+++ django_project/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+    'storages'
 ]
 
 MIDDLEWARE = [
@@ -143,3 +143,13 @@ EMAIL_HOST_PASSWORD = os.environ.get('GMAIL_PASS')
 
 
 
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+AWS_S3_REGION_NAME = 'eu-west-2'
+#AWS_S3_SIGNATURE_VERSION = 's3v4'
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
