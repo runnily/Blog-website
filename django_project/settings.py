424f1c09f4c5bdcdbc5a08b5a0d3786a4b08a8b4
runnily <32201807+runnily@users.noreply.github.com>
2020-10-05 20:07:15 +0100

    updated settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'hyr($6qpycz+a_nstg!45nswjtlobtg-ygmvz9(jrbt5ntc@$u'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG_VALUE')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['djangoexp.herokuapp.com']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'storages'
+    'djangoexp.herokuapp.com'
 ]
 
 MIDDLEWARE = [
